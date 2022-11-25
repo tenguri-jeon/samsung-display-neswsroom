993a26896b14f6209f510ed50cff8e52cc1cf9b3
tenguri-jeon <110875363+tenguri-jeon@users.noreply.github.com>
2022-11-25 12:51:17 +0900

    x

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ const imgContainerwrapper = document.querySelector('.img-container-monitor');
 console.log(imgContainerwrapper);
 
 
-for(let i=0 ; i < ProgressButton.length ; i++ ){
-    createBar();
-}
+// for(let i=0 ; i < ProgressButton.length ; i++ ){
+//     createBar();
+// }
 
 function createBar() {
     const makeProgressButton = document.createElement('div');
@@ -29,3 +29,20 @@ function createBar() {
     makeProgressButton.appendChild(makeProgressBar);
     document.querySelector('.buttons').appendChild(makeProgressButton);
 }
+
+
+// 인기기사
+const topnewsContainerwrapper = document.getElementsByClassName('top-news-container-wrapper');
+const topNewscontainer = document.getElementsByClassName('top-news-container');
+const slideCount = topNewscontainer[0].childElementCount;
+const leftbutton = document.getElementsByClassName('left');
+const rightbutton = document.getElementsByClassName('right');
+
+for(let i = 0 ; i < topnewsContainerwrapper.length; i ++){
+    topNewscontainer[i].style.width = `${100/slideCount}%`
+}
+
+leftbutton.addEventListener('click' , ()=>{
+    topNewscontainer.style.transform = translateX(`${100/slideCount}%`)
+})
+
