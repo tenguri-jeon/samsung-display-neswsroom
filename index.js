d81ed4cc034ae7317ddff14742577fecbe03a04f
tenguri-jeon <110875363+tenguri-jeon@users.noreply.github.com>
2022-11-28 12:52:03 +0900

    gvhb

--- index.js
+++ index.js
@@ -1,78 +1,19 @@
-const navTouch = document.querySelector('nav');
-const header = document.querySelector('header');
-
-
-
-navTouch.addEventListener('mouseenter', ()=>{
-    header.classList.add('active');
-})
-
-navTouch.addEventListener('mouseleave', ()=>{
-    header.classList.remove('active');
-})
-// 네비게이션 스크립트
-
-const index = 0;
-const imgContainerwrapper = document.querySelector('.img-container-monitor');
-console.log(imgContainerwrapper);
-
-
-// for(let i=0 ; i < ProgressButton.length ; i++ ){
-//     createBar();
-// }
-
-function createBar() {
-    const makeProgressButton = document.createElement('div');
-    makeProgressButton.classList.add('progress-button')
-    const makeProgressBar = document.createElement('span');
-    makeProgressBar.classList.add('progress-span');
-    makeProgressButton.appendChild(makeProgressBar);
-    document.querySelector('.buttons').appendChild(makeProgressButton);
-}
-
-
 // 인기기사
-const topNewscontainer = document.getElementsByClassName('top-news-container');
-const leftbutton = document.getElementsByClassName('before-img');
-const rightbutton = document.getElementsByClassName('after-img');
-const topNewscontainerwrapper = document.getElementsByClassName('top-news-container-wrapper');
-const slideCount = topNewscontainerwrapper[0].childElementCount;
 
-leftrightbutton();
+//한개의 컨텐츠width는 -> (전체 크기 - (갭크기*슬라이드갯수-1))/슬라이드 갯수
+// (1200px - (32px*3-1))/3
+const slide = document.getElementsByClassName('top-news-slidewapper');
+const slideWidth = document.getElementsByClassName('top-news-container');
+const containerWidth = slide.clientWidth;
+const count = slide.childElementCount;
 
-function leftrightbutton () {
-    leftbutton.addEventListener('click', slideLeft);
-    rightbutton.addEventListener('click', slideRight);
-
-//슬라이드 레퍼의 크기 조정해 주기 => 화면 형성해 주기
-    for (let i = 0; i < topNewscontainerwrapper.length; i++) {
-        topNewscontainerwrapper[i].style.width = `${slideCount*100}%`
-    }
-//슬라이드 레퍼의 크기 조정해 주기 => calc(100 / 3과 같음)
-    for(let i = 0 ; i < topNewscontainer.length ; i ++) {
-        topNewscontainer[i].style.width = `${100/slideCount}%`
-    }
-
-function slideRight() {
-    index++;
-    if(index===slideCount){
-        index=0;
-    }
-    applySlide();
+function width() {
+    slideWidth = (containerWidth - (32*(count-1))/count);
 }
 
-function slideLeft() {
-    index--;
-    if(index === -1){
-        index = slideCount-1;
-    }
-    applySlide();
-}
 
-function applySlide() {
-    for (let i = 0; i < topNewscontainerwrapper.length; i++) {
-    topNewscontainerwrapper[i].style.transform = `translateX(calc( -100% / ${slideCount} * ${index}))`;
-}
-}
-}
+
+
+
+
 
