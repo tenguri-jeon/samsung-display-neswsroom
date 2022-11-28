ce8de6e93df92c9adfc27fc971b1b93309d9c826
tenguri-jeon <110875363+tenguri-jeon@users.noreply.github.com>
2022-11-28 23:10:38 +0900

    11

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-// 인기기사
+chechWidth();
+// 인기기사 js는 되는데 사진이 잘려요....
 
 //한개의 컨텐츠width는 -> (전체 크기 - (갭크기*슬라이드갯수-1))/슬라이드 갯수
 // (1200px - (32px*3-1))/3
-const slide = document.getElementsByClassName('top-news-slidewapper');
-const slideWidth = document.getElementsByClassName('top-news-container');
-const containerWidth = slide.clientWidth;
-const count = slide.childElementCount;
 
-function width() {
-    slideWidth = (containerWidth - (32*(count-1))/count);
+let containerWidth;
+let slideWidth;
+let index = 0;
+const slidecontainerWidth = document.getElementById('top-news-wrapper');
+const slide = document.getElementsByClassName('top-news-container');
+const slideWrapper = document.getElementsByClassName('top-news-container-wrapper');
+const count = 5;
+const nextslide = document.getElementsByClassName('after-img');
+
+// nextslide[1].addEventListener('click', next);
+
+function chechWidth() {
+    // 컨테이너 전체 크기
+    slideWidth = (containerWidth - (64 * (count - 1))) / count;
+    containerWidth = slidecontainerWidth.clientWidth;
+    for (let i = 0; i < count; i++) {
+        slide[i].style.width = `${slideWidth}px`;
+    }
 }
 
-
+// 왜 안될까요
+// function next() {
+//     if(index < slideWrapper.childElementCount-count){
+//         index ++;
+//     slideWrapper.style.transfrom = `translateX(-${index * (slideWidth + 32)}px)`;
+// }
+// }
 
 
 
