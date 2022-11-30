42af711eaf65bad1b497d050da42ac97adacd68a
tenguri-jeon <110875363+tenguri-jeon@users.noreply.github.com>
2022-11-30 12:49:43 +0900

    55

--- js/main-slide.js
+++ js/main-slide.js
@@ -1,12 +1,20 @@
+mainslide();
+function mainslide() {
+    
+    
 const maincontainerwidth = document.getElementsByClassName('img-container-monitor');
-// slideCount가 이상한지 다음으로 이상하게 넘어갑니다.
 const slideCount = 3;
 const maincontainer = document.getElementsByClassName('img-container');
 const mainslideWrapper = document.getElementsByClassName('img-container-wrapper');
+const nextslide = document.getElementsByClassName('after-img');
+const beforeslide = document.getElementsByClassName('before-img');
+const navigationBar = document.getElementsByClassName('progress-span');
+const navigationbutton = document.getElementsByClassName('progress-button');
+let slideWith ; 
+let index = 0;
+let barWith = 0;
+let barInterval;
 
-
-
-// 네비게이션바 만들기
 for (let i = 0; i < maincontainer.length; i++) {
     makeProgressButton();
 }
@@ -19,6 +27,7 @@ function makeProgressButton(){
     navigationbutton.appendChild(navigationBar);
     document.querySelector('.buttons').appendChild(navigationbutton);
 }
+// 네비게이션바 만들기
 
 
 
@@ -26,18 +35,19 @@ function makeProgressButton(){
 
 // 각 컨테이너 크기 만들어주기
 for (let i = 0; i < maincontainer.length; i++) {
-    maincontainer[i].style.width = `${100 / slideCount}`
+    maincontainer[i].style.width = `${100 / slideCount}%`
 }
 
 // 컨테이너 크기 만들어주기 -> 컨테이너의 크기 * 100
-for (let i = 0; i < maincontainerwidth.length; i++) {
-    maincontainerwidth[i].style.width = `${slideCount * 100}%`
-}
-
-// 클릭했을때 넘어가는 이벤트 만들기
-nextslide[0].addEventListener('click', slideRight);
-beforeslide[0].addEventListener('click', slideleft);
-
+// for (let i = 0; i < maincontainerwidth.length; i++) {
+    //     maincontainerwidth[i].style.width = `${slideCount * 100}%`
+    // }
+    
+    
+    // 클릭했을때 넘어가는 이벤트 만들기
+    nextslide[0].addEventListener('click', slideRight);
+    beforeslide[0].addEventListener('click', slideleft);
+    
 function slideRight() {
     index++;
     if (index===slideCount) {
@@ -48,7 +58,7 @@ function slideRight() {
 
 function slideleft() {
     index--;
-    if (index===-1) {
+    if (index=== -1 ) {
         index = slideCount -1 ;
     }
     applySlide();
@@ -56,9 +66,49 @@ function slideleft() {
 
 
 function applySlide() {
+    resetBar();
     for (let i = 0; i < mainslideWrapper.length ; i++) {
-        index++;
-        mainslideWrapper[i].style.transform = `translateX(calc( -100% / ${slideCount} * ${index}))`
+        mainslideWrapper[i].style.transform = `translateX(-${100 / slideCount * index}%)`
     }
+}
+
+// 네비게이션바 span 크기 바꾸기 
+/*
+1. 스팸 가져오기 -> function안에 들어가 있으니 다시 가져와야함
+2. 스팸 크기 무엇으로 늘리고 줄일지 -> 
+스팸 가져와서 윗스를 0%로 만들고 상황에 적절한 곳에 클래스 active넣어줌
+intervarl을 선언하고 setInterval사용하여 interval을 실행시킨다. 
+3. 스팸과 각 컨텐츠를 연겷해서 누르면 컨텐츠로 이동하게 만들어보기
+*/
+
+resetBar();
+startBar();
+
+function resetBar() {
+    for (let i = 0; i < navigationbutton.length; i++) {
+        barWith = 0;
+        navigationbutton[i].classList.remove('active');
+        navigationBar[i].style.width = '0%';
+    }
+    navigationbutton[index].classList.add('active');
+}
+
+function startBar() {
+    barInterval = setInterval(() => {
+        growBar();
+    }, 100);
+}
+
+function stopBar() {
+    clearTimeout(barInterval);
+}
+
+function  growBar() {   
+    barWith += 2;    
+    navigationBar[index].style.width = `${barWith}%`;
+    if(barWith===100) {
+        slideRight()
+    }
+}
 }
 
--- js/top-news.js
+++ js/top-news.js
@@ -2,6 +2,8 @@
 
 //한개의 컨텐츠width는 -> (전체 크기 - (갭크기*슬라이드갯수-1))/슬라이드 갯수
 // (1200px - (32px*3-1))/3
+인기기사슬라이드();
+function 인기기사슬라이드(){
 
 let containerWidth;
 let slideWidth;
@@ -25,6 +27,13 @@ nextslide[1].addEventListener('click', (e)=>{
 }
 );
 
+beforeslide[1].addEventListener('click' , (e)=>{
+    e.preventDefault();
+    leftslide();
+}
+);
+
+
 chechWidth(3, slidecontainerWidth, slide);
 
 function chechWidth(count, slidehallwidth, contents) {
@@ -45,7 +54,12 @@ function next() {
     // 클래스네임으로 가져왔기 때문에 [0]해줘야함
     slideWrapper[0].style.transform = `translateX(-${index * (slideWidth + 32)}px)`;
 }
+}
 
+function leftslide() {
+    index--;
+    slideWrapper[0].style.transform = `translateX(-${index * (slideWidth + 32)}px)`;
+}
 
 
 
